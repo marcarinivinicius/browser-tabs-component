f6fbaeddd820a8dbf5a75de8d9a8bc96596f90c5
Vinícius A. Marcarini <165974874+marcarinivinicius@users.noreply.github.com>
2025-03-18 15:15:21 -0300

    Create dynamic-component-loader.service.ts

---
+++ src/app/services/dynamic-component-loader.service.ts
@@ -0,0 +1,27 @@
+﻿import { ComponentRef, Injectable, Injector, Type, ViewContainerRef } from '@angular/core';
+import { TestComponent } from '../example/test/test.component';
+import { Test1Component } from '../example/test1/test1.component';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class DynamicComponentLoaderService {
+    private componentMap: Record<string, Type<any>> = {
+        'TestComponent': TestComponent,
+        'Test1Component': Test1Component
+    };
+
+    constructor(private injector: Injector) {}
+
+    loadComponent(componentName: string, viewContainerRef: ViewContainerRef): ComponentRef<any> | null {
+        const componentType = this.componentMap[componentName];
+
+        if (!componentType) {
+            console.error(`Componente ${componentName} não encontrado.`);
+            return null;
+        }
+
+        viewContainerRef.clear(); // Remove qualquer componente anterior antes de injetar um novo
+        return viewContainerRef.createComponent(componentType, { injector: this.injector });
+    }
+}
